d22a0802d6dbe82f8d041369013be2612c2b41e9
Andrei Burvel <burvelandrei@gmail.com>
2024-09-15 15:50:44 +0300

    добавлены методы классу Warehouse

--- dz_12_1.py
+++ dz_12_1.py
@@ -1,24 +1,5 @@
 from dataclasses import dataclass
 
-# class Product:
-#     def __init__(self, name: str, store_name: str, price: float):
-#         self.__name = name
-#         self.__store_name = store_name
-#         self.__price = price
-
-#     @property
-#     def name(self):
-#         return self.__name
-
-
-#     @property
-#     def store_name(self):
-#         return self.__store_name
-
-
-#     @property
-#     def price(self):
-#         return self.__price
 
 @dataclass
 class Product:
@@ -34,7 +15,6 @@ class Product:
     def store_name(self):
         return self.__store_name
 
-
     @property
     def price(self):
         return self.__price
@@ -45,21 +25,53 @@ class Warehouse:
     def __init__(self, *args):
         self.__products = args
 
+    def output_by_index(self, find_index):
+        try:
+            product = self.__products[find_index]
+            print(
+                "Информация:\n"
+                f"Наименование товара: {product.name}\n"
+                f"Магазин продающий товар: {product.store_name}\n"
+                f"Стоимость товара: {product.price} руб"
+            )
+        except IndexError:
+            print("Такой индекс отсутствует!!")
+
     def output_by_name(self, find_name):
         for product in self.__products:
             if product.name.lower() == find_name.lower():
-                print("Информация:\n"
-                      f"Наименование товара: {product.name}\n"
-                      f"Магазин продающий товар: {product.store_name}\n"
-                      f"Стоимость товара: {product.price}")
+                print(
+                    "Информация:\n"
+                    f"Наименование товара: {product.name}\n"
+                    f"Магазин продающий товар: {product.store_name}\n"
+                    f"Стоимость товара: {product.price} руб"
+                )
+                break
+        else:
+            print("Такое наименование отсутсвует на складе!!")
 
+    def sort_by_name(self):
+        self.__products = sorted(
+            self.__products, key=lambda product: product.name.lower()
+        )
 
+    def sort_by_store_name(self):
+        self.__products = sorted(
+            self.__products, key=lambda product: product.store_name.lower()
+        )
 
+    def sort_by_price(self):
+        self.__products = sorted(self.__products, key=lambda product: product.price)
 
 
 orange = Product("Апельсин", "Пятёрочка", 5)
 apple = Product("Яблоко", "Евроопт", 4)
 milk = Product("Молоко", "Виталюр", 3)
+banan = Product("Банан", "Санта", 8)
 
-warehouse = Warehouse(orange, apple, milk)
+warehouse = Warehouse(orange, apple, milk, banan)
 warehouse.output_by_name("яблоко")
+warehouse.output_by_index(2)
+warehouse.sort_by_name()
+warehouse.sort_by_store_name()
+warehouse.sort_by_price()
