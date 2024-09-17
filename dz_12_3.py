0584b701f6e1f395c3a391ebeda1da5d0de35a8e
Andrei Burvel <burvelandrei@gmail.com>
2024-09-18 01:49:09 +0300

    исправил методы и добавил документацию

--- dz_12_3.py
+++ dz_12_3.py
@@ -1,83 +1,111 @@
 class Bus:
+    """
+    Класс описывающий Автобус. Имеет атрибуты скорости,
+    максимальной вместимости, максимальной скорости, список
+    фамилий пассажиров и словарь мест.
+    Реализованы методы изменения скорости(сбросить/добавить),
+    посадки/высадки пассажиров, посадки/высадки пассажира по фамилии,
+    проверка на присутствие пассажира в автобусе
+    """
 
-    def __init__(self, speed: int, max_count_seats: int, max_speed: int, list_passengers_name: list, vakancy_flag: bool):
-        self._speed = speed
+    def __init__(self, max_count_seats: int, max_speed: int):
+        self._speed = 0
         self._max_count_seats = max_count_seats
         self._max_speed = max_speed
-        self._list_passengers_name = list_passengers_name
-        self._vakancy_flag = vakancy_flag
+        self._list_passengers_name = []
+        self._vakancy_flag = True
+        self._dict_of_seats = {key: None for key in range(1, max_count_seats + 1)}
 
     @property
     def speed(self):
         return self._speed
 
     @property
-    def max_count_seats(self):
-        return self._max_count_seats
-
-    @property
-    def max_speed(self):
-        return self._max_speed
-
-    @property
     def list_passengers_name(self):
         return self._list_passengers_name
-
-    @property
-    def vakancy_flag(self):
-        return self._vakancy_flag
 
     def reduce_speed(self, value):
         self._speed = self._speed - value if self._speed - value > 0 else 0
 
     def increase_speed(self, value):
-        self._speed = self._speed + value if self._speed + value < self._max_speed else self._max_speed
+        self._speed = (
+            self._speed + value
+            if self._speed + value < self._max_speed
+            else self._max_speed
+        )
 
     def boarding_passengers(self, *args):
-        if len(self._list_passengers_name) + len(args) <= self.max_count_seats:
-            self._list_passengers_name += args
-        self._vakancy_flag = True if len(self._list_passengers_name) < self._max_count_seats else False
+        for passenger in args:
+            if len(self._list_passengers_name) < self._max_count_seats:
+                for seat, value in self._dict_of_seats.items():
+                    if value is None:
+                        self._dict_of_seats[seat] = passenger
+                        self._list_passengers_name.append(passenger)
+                        break
+            else:
+                self._vakancy_flag = False
+                print("В автобусе нет мест!")
+                break
 
     def disembarkation_of_passengers(self, *args):
-        for name_passenger in args:
-            self._list_passengers_name.remove(name_passenger)
-        self._vakancy_flag = True if len(self._list_passengers_name) < self._max_count_seats else False
+        for passenger in args:
+            for seat, value in self._dict_of_seats.items():
+                if value == passenger:
+                    self._dict_of_seats[seat] = None
+                    self._list_passengers_name.remove(passenger)
+        self._vakancy_flag = (
+            True if len(self._list_passengers_name) < self._max_count_seats else False
+        )
 
     def __iadd__(self, other):
         if len(self._list_passengers_name) < self._max_count_seats:
-            self._list_passengers_name.append(other)
-            self._vakancy_flag = True if len(self._list_passengers_name) < self._max_count_seats else False
+            for seat, value in self._dict_of_seats.items():
+                if value is None:
+                    self._dict_of_seats[seat] = other
+                    self._list_passengers_name.append(other)
             return self
-        self._vakancy_flag = True if len(self._list_passengers_name) < self._max_count_seats else False
+        self._vakancy_flag = (
+            True if len(self._list_passengers_name) < self._max_count_seats else False
+        )
         return self
 
     def __isub__(self, other):
-        self._list_passengers_name.remove(other)
-        self._vakancy_flag = True if len(self._list_passengers_name) < self._max_count_seats else False
+        for seat, value in self._dict_of_seats.items():
+            if value == other:
+                self._dict_of_seats[seat] = None
+                self._list_passengers_name.remove(other)
+        self._vakancy_flag = (
+            True if len(self._list_passengers_name) < self._max_count_seats else False
+        )
         return self
 
     def __contains__(self, other):
         return other in self._list_passengers_name
 
+    def __str__(self) -> str:
+        return f"{self._speed}|{self._list_passengers_name}|{self._dict_of_seats}"
 
 
-
-bus = Bus(45, 5, 100, ["Иванов", "Петров", "Смирнов", "Гришин"], True)
+bus = Bus(3, 100)
 print("-" * 50)
 bus.increase_speed(65)
 print(bus.speed)
 print("-" * 50)
-bus.boarding_passengers("Бурвель", "Вакульчик")
+bus.reduce_speed(15)
+print(bus.speed)
+print("-" * 50)
+bus.boarding_passengers("Бурвель", "Вакульчик", "Иванов")
 print(bus.list_passengers_name)
 print("-" * 50)
-bus.disembarkation_of_passengers("Гришин", "Смирнов")
+bus.disembarkation_of_passengers("Вакульчик")
 print(bus.list_passengers_name)
 print("-" * 50)
 bus += "Рыбин"
 print(bus.list_passengers_name)
 print("-" * 50)
-bus -= "Иванов"
+bus -= "Бурвель"
 print(bus.list_passengers_name)
 print("-" * 50)
 print("Петров" in bus)
 print("-" * 50)
+print(bus)
